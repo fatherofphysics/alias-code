8d33385c620cec1fa74615a1b8f23bd7ecbc265a
Vedant Kanoujia <68261108+fatherofphysics@users.noreply.github.com>
2022-04-06 20:05:23 +0530

    Update .alias.sh

--- .alias.sh
+++ .alias.sh
@@ -8,7 +8,7 @@ alias ...="cd ../../../"
 alias ....="cd ../../../../"
 alias .....="cd ../../../../"
 alias cls="clear"
-alias rf="rm -rf"
+alias rf="sudo rm -rf"
 alias e="logout"
 alias q="exit"
 alias x="nmap"
@@ -17,8 +17,8 @@ alias p3="python3"
 alias p2="python2"
 
 # create sample file fast
-alias txt="touch sample.txt"
-alias bsh="touch sample.sh && chmod +x sample.sh"
+alias txt="sudo touch sample.txt"
+alias bsh="sudo touch sample.sh && chmod +x sample.sh"
 
 # print cuurent dir
 alias p="pwd | lolcat"
@@ -27,8 +27,8 @@ alias p="pwd | lolcat"
 alias mi="micro"
 
 # File creation
-alias md="mkdir"
-alias mf="touch"
+alias md="sudo mkdir"
+alias mf="sudo touch"
 
 # Use git clone fast
 alias gc="git clone"
@@ -46,14 +46,14 @@ alias root="sudo su"
 # run cmd with sudo
 alias s="sudo"
 
-## Pkg install & system update 
+## Pkg install & system update
 alias i="sudo apt install"
 alias up="sudo apt update"
 alias ug="sudo apt upgrade"
 alias rv="sudo apt remove"
 
 # Stop after sending count ECHO_REQUEST packets
-alias ping="ping -c 5"
+alias pi="sudo ping -c 3"
 
 # Do not wait interval 1 second, go fast
-alias fastping="ping -c 100 -s.2"
+alias fastpi="sudo ping -i 0.1 -c 5"
